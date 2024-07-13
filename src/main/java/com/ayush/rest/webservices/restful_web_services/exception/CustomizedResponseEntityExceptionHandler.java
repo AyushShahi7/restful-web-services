8f5beb06842b085f7125440595ea1a3a9230ae88
Ayush <ayushshai123@gmail.com>
2024-07-13 14:17:42 +0530

    Getting java: No enum constant javax.lang.model.element.Modifier.SEALED, Implementing Validations for REST API skipped

--- src/main/java/com/ayush/rest/webservices/restful_web_services/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/ayush/rest/webservices/restful_web_services/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,11 +1,8 @@
 package com.ayush.rest.webservices.restful_web_services.exception;
 
 import com.ayush.rest.webservices.restful_web_services.user.UserNotFoundException;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
